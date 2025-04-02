55dd182c24fc64685d7fbd4c97f2814c7425a2ad
Hiteshkun21 <115580771+Hiteshkun21@users.noreply.github.com>
2025-04-02 22:50:01 +0530

    new error resolved

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main (String [] args ){
      System.out.println("this is jenkins project");
-     System.out.println("this is add comment")
+     System.out.println("this is add comment");
     }
 }
