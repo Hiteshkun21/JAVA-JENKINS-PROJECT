2f93cfa60864199abdc00e97be6ce3e72c1d4760
Hiteshkun21 <115580771+Hiteshkun21@users.noreply.github.com>
2025-04-02 22:52:23 +0530

    autometed code

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main
     public static void main (String [] args ){
      System.out.println("this is jenkins project");
      System.out.println("this is add comment");
+     System.out.println("autometed jenkins ");
     }
+
 }
