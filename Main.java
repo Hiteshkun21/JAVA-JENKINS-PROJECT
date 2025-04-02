c946bf33711ab002aa947a00a25fab20db4611d1
Hiteshkun21 <115580771+Hiteshkun21@users.noreply.github.com>
2025-04-02 23:13:58 +0530

    arithematic operation

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main
      System.out.println("this is jenkins project");
      System.out.println("this is add comment");
      System.out.println("autometed jenkins ");
+     System.out.println(10+2);
     }
 
 }
